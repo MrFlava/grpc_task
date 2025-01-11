deee79acd0f293ecb88ee64f26ebad6399f91191
MrFlava <thatelitemaili33t@gmail.com>
2025-01-11 12:40:21 +0200

    Work on the task_server

--- task_server.py
+++ task_server.py
@@ -5,3 +5,25 @@ import grpc
 import task_pb2
 import task_pb2_grpc
 
+class SimilaritySearchServiceServicer(task_pb2_grpc.SimilaritySearchServiceServicer):
+    def AddItem(self, request, context):
+        print("Adding item request")
+        print(request)
+
+        add_item_reply = task_pb2.AddItemResponse()
+        add_item_reply.status = 1
+        add_item_reply.message = "created"
+
+        return add_item_reply
+
+    def SearchItems(self, request, context):
+        print("Searching items request")
+        print(request)
+
+        search_item_reply = task_pb2.SearchItemsResponse()
+        search_item_reply.search_id = 'test-id'
+
+        return search_item_reply
+
+    def GetSearchResults(self, request, context):
+        pass
