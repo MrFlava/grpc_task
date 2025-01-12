b76b4965b32e5cf9c8f717e9e280fb39975ac528
MrFlava <thatelitemaili33t@gmail.com>
2025-01-12 21:35:26 +0200

    Work on the client

--- task_client.py
+++ task_client.py
@@ -19,21 +19,22 @@ def run():
         print("2. Search items")
         print("3. Get search results")
 
-        # rpc_call = input("Which rpc you like to make: ")
+        rpc_call = input("Which rpc you like to make: ")
 
-        # if rpc_call == "1":
-        #     hello_request = test_pb2.CoolRequest(greeting = "Bonjour", name = "You")
-        #     hello_reply = stub.SayCool(hello_request)
-        #     print("SayCool Response Received:")
-        #     print(hello_reply)
-        # elif rpc_call == "2":
-        #     hello_request = test_pb2.CoolRequest(greeting = "Bonjour", name = "Youuu")
-        #     hello_replies = stub.SimonSaysHello(hello_request)
-        #
-        #     for hello_reply in hello_replies:
-        #         print("SimonSaysHello Response Received:")
-        #         print(hello_reply)
-        # elif rpc_call == "3":
+        if rpc_call == "1":
+            pass
+            add_request = task_pb2.AddItemRequest(id="test1", description="test")
+            add_response = stub.AddItem(add_request)
+            print("Addr Response Received:")
+            print(add_response)
+        elif rpc_call == "2":
+            search_items_request = task_pb2.SearchItemsRequest(query="test")
+            search_items_response = stub.SearchItems(search_items_request)
+
+            print("Search Items Response Received:")
+            print(search_items_response)
+        elif rpc_call == "3":
+            pass
         #     delayed_reply = stub.SimonClientSaysHello(get_client_stream_requests())
         #
         #     print("SimonClientSaysHello Response Received:")
